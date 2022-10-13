532a242c4df2158d24f1bf60fd71ba4bc98a47f0
linmenghao <linmenghao20001002@163.com>
2022-10-13 10:17:58 +0800

    多数据源配置，mybatis-plus代码生成

--- base-service/src/main/java/com/xzkj/baseservice/BaseServiceApplication.java
+++ base-service/src/main/java/com/xzkj/baseservice/BaseServiceApplication.java
@@ -2,8 +2,12 @@ package com.xzkj.baseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = {"com.xzkj"})
 public class BaseServiceApplication {
 
     public static void main(String[] args) {
